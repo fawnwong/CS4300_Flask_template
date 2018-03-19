ffa0eb1f6c816de16a7301ca28998e7bf8b18997
fawnwong <fawn.wong@live.com>
2018-03-18 22:07:44 -0400

    initial setup completed

--- app/irsystem/__init__.py
+++ app/irsystem/__init__.py
@@ -4,4 +4,7 @@ from flask import Blueprint
 irsystem = Blueprint('irsystem', __name__, url_prefix='/',static_folder='static',template_folder='templates')
 
 # Import all controllers
-from controllers.search_controller import *
+try: 
+	from controllers.search_controller import *
+except ImportError:
+	from app.irsystem.controllers.search_controller import *
