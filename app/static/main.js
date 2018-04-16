8f1ae5295c7bae680753fd7d5ceb6a5099595e5f
Annie Zhang <zz229@cornell.edu>
2018-04-16 17:06:28 -0400

    animate results

---
+++ app/static/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  if(window.location.href.indexOf("search") > -1) {
+    // window.location.href = (String(window.location.href) + "#results");
+    $('html, body').animate({
+      scrollTop: $('#output').offset().top
+    }, 100);
+  }
+});
