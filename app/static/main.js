2c2fd7967c145dfef69b3c8d475e060fe30e0299
fawnwong <fawn.wong@live.com>
2018-04-23 11:00:47 -0400

    testing m2

--- app/static/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-  if(window.location.href.indexOf("search") > -1) {
-    // window.location.href = (String(window.location.href) + "#results");
-    $('html, body').animate({
-      scrollTop: $('#output').offset().top
-    }, {duration: 600, easing: 'swing'});
-  }
-});
