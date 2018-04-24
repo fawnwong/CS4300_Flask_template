3d548da4b588575e6ff7d7026b565dd613727656
Annie Zhang <zz229@cornell.edu>
2018-04-23 22:33:24 -0400

    one search at a time

--- app/irsystem/models/search.py
+++ app/irsystem/models/search.py
@@ -81,53 +81,69 @@ def getUserCommentResults(query, bot_names_list, user_comments):
 	return False
 
 
-def bot_to_list(query):
+def bot_to_list(query, query_type):
 	if query == None:
 		return []
-	edit_dist = similar_names(query, bot_names)
-	cos_sim = top_n_cos(5,query, tfidf_vec)
-
-	bot_names_list = 'bot_names.csv'
-	user_comments = 'user_comment_data.csv'
-
-	query_words = query.split()
-	for query in query_words:
-		myresults = getUserCommentResults(query, bot_names_list, user_comments)
-		if myresults:
-			break
-
-	if not myresults:
-		myresults = [("no category",0) , ("no category",0) , ("no category",0) , ("no category",0) , ("no category", 0)]
-		
-	data = [{"rank": "1", "list": [{"name": edit_dist[0][1], "comment": "A Comment 1", "link": "http://reddit.com/u/"+ edit_dist[0][1], "category": "bot_name"},
-								   {"name": cos_sim[0][0], "comment": "B Comment 1", "link": "http://reddit.com/u/"+cos_sim[0][0], "category": "bot_comments"},
-								   {"name": myresults[0][0], "comment": "C Comment 1", "link": "http://reddit.com/u/" + myresults[0][0], "category": "user_comments"}
-								   ]},
+	if query_type == "name":
+		edit_dist = similar_names(query, bot_names)
+		data = [{"rank": "1", "result": {"name": edit_dist[0][1], "comment": "A Comment 1", "link": "http://reddit.com/u/"+ edit_dist[0][1], "category": "bot_name"}},
 			{"rank": "2",
-			"list": [
-					 {"name": edit_dist[1][1], "comment": "A Comment 2", "link": "http://reddit.com/u/" + edit_dist[1][1], "category": "bot_name"},
-					 {"name": cos_sim[1][0], "comment": "B Comment 2", "link": "http://reddit.com/u/"+cos_sim[1][0], "category": "bot_comments"},
-					 {"name": myresults[1][0], "comment": "C Comment 2", "link": "http://reddit.com/u/" + myresults[1][0], "category": "user_comments"}
-					 ]},
+			"result":{"name": edit_dist[1][1], "comment": "A Comment 2", "link": "http://reddit.com/u/" + edit_dist[1][1], "category": "bot_name"}
+					 },
 			{"rank": "3",
-			"list": [
-					 {"name": edit_dist[2][1], "comment": "A Comment 3", "link": "http://reddit.com/u/" + edit_dist[2][1], "category": "bot_name"},
-					 {"name": cos_sim[2][0], "comment": "B Comment 3", "link": "http://reddit.com/u/"+cos_sim[2][0], "category": "bot_comments"},
-					 {"name": myresults[2][0], "comment": "C Comment 3", "link": "http://reddit.com/u/" + myresults[2][0], "category": "user_comments"}
-					 ]},
+			"result": {"name": edit_dist[2][1], "comment": "A Comment 3", "link": "http://reddit.com/u/" + edit_dist[2][1], "category": "bot_name"}
+					 },
 			{"rank": "4",
-			"list": [
-					 {"name": edit_dist[3][1], "comment": "A Comment 4", "link": "http://reddit.com/u/" + edit_dist[3][1], "category": "bot_name"},
-					 {"name": cos_sim[3][0], "comment": "B Comment 4", "link": "http://reddit.com/u/"+cos_sim[3][0], "category": "bot_comments"},
-					 {"name": myresults[3][0], "comment": "C Comment 4", "link": "http://reddit.com/u/" + myresults[3][0], "category": "user_comments"}
-					 ]},
+			"result": {"name": edit_dist[3][1], "comment": "A Comment 4", "link": "http://reddit.com/u/" + edit_dist[3][1], "category": "bot_name"}
+					 },
 			{"rank": "5",
-			"list": [
-					 {"name": edit_dist[4][1], "comment": "A Comment 5", "link":"http://reddit.com/u/" + edit_dist[4][1], "category": "bot_name"},
-					 {"name": cos_sim[4][0], "comment": "B Comment 5", "link": "http://reddit.com/u/"+cos_sim[4][0], "category": "bot_comments"},
-					 {"name": myresults[4][0], "comment": "C Comment 5", "link": "http://reddit.com/u/" + myresults[4][0], "category": "user_comments"}
-					 ]},
+			"result": {"name": edit_dist[4][1], "comment": "A Comment 5", "link":"http://reddit.com/u/" + edit_dist[4][1], "category": "bot_name"}
+					 }
 			]
+	elif query_type == "bot-com":
+		cos_sim = top_n_cos(5,query, tfidf_vec)
+		data = [{"rank": "1", "result": {"name": cos_sim[0][0], "comment": "B Comment 1", "link": "http://reddit.com/u/"+cos_sim[0][0], "category": "bot_comments"}},
+			{"rank": "2",
+			"result":{"name": cos_sim[1][0], "comment": "B Comment 2", "link": "http://reddit.com/u/"+cos_sim[1][0], "category": "bot_comments"}
+					 },
+			{"rank": "3",
+			"result": {"name": cos_sim[2][0], "comment": "B Comment 3", "link": "http://reddit.com/u/"+cos_sim[2][0], "category": "bot_comments"}
+					 },
+			{"rank": "4",
+			"result": {"name": cos_sim[3][0], "comment": "B Comment 4", "link": "http://reddit.com/u/"+cos_sim[3][0], "category": "bot_comments"}
+					 },
+			{"rank": "5",
+			"result": {"name": cos_sim[4][0], "comment": "B Comment 5", "link": "http://reddit.com/u/"+cos_sim[4][0], "category": "bot_comments"}
+					 }
+			]
+	else:
+		bot_names_list = 'bot_names.csv'
+		user_comments = 'user_comment_data.csv'
+
+		query_words = query.split()
+		for query in query_words:
+			myresults = getUserCommentResults(query, bot_names_list, user_comments)
+			if myresults:
+				break
+
+		if not myresults:
+			myresults = [("no category",0) , ("no category",0) , ("no category",0) , ("no category",0) , ("no category", 0)]
+
+		data = [{"rank": "1", "result": {"name": myresults[0][0], "comment": "C Comment 1", "link": "http://reddit.com/u/" + myresults[0][0], "category": "user_comments"}},
+			{"rank": "2",
+			"result":{"name": myresults[1][0], "comment": "C Comment 2", "link": "http://reddit.com/u/" + myresults[1][0], "category": "user_comments"}
+					 },
+			{"rank": "3",
+			"result": {"name": myresults[2][0], "comment": "C Comment 2", "link": "http://reddit.com/u/" + myresults[2][0], "category": "user_comments"}
+					 },
+			{"rank": "4",
+			"result": {"name": myresults[3][0], "comment": "C Comment 2", "link": "http://reddit.com/u/" + myresults[3][0], "category": "user_comments"}
+					 },
+			{"rank": "5",
+			"result": {"name": myresults[4][0], "comment": "C Comment 2", "link": "http://reddit.com/u/" + myresults[4][0], "category": "user_comments"}
+					 }
+			]
+			
 	return data
 
 
